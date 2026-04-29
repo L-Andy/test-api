f13dadc83dbd79b55a40cb304e6fbbfe3a8bba08
L-Andy <lutaayaandrew200@gmail.com>
2026-04-29 05:18:55 +0300

    Updating port

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Starting Wellness Connect API..."
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} --workers 1
+exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-6000} --workers 1
